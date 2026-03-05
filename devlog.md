3212aba6e44ba29dfa1a790e085d08f56c0fc278
Himesh A <himesha@Himeshs-MacBook-Pro.local>
2026-03-05 17:11:34 -0600

    Commit 1: basic stuff done

--- devlog.md
+++ devlog.md
@@ -1,3 +1,8 @@
 3/05/2026 4:30 pm
 
 This is my first commit. I made the mistake of starting kind of late because this is due tomorrow but I think if I take my time I can finish this. The program needs to be able to support batch modes. The calculator needs to read prefix notation from left to right and the user can type quit to exit the program. There should be a history and be able to handle errors. 
+
+
+
+3/05/2026 5:10 pm 
+This is the end of my first work session. I wrote the batch detection and started with the processing of user input. 
--- main.rkt
+++ main.rkt
@@ -1,2 +1,36 @@
 #lang racket
-(displayln "Hello, World!")
+
+
+
+;batch mode dectection
+
+(define interactive?
+  (let [(args (current-command-line-arguments))]
+    (not (or (vector-member "-b" args)
+             (vector-member "--batch" args)))))
+
+
+
+;skip spaces 
+
+(define (skip-space lst)
+  (cond
+    [(not (list? lst)) #f]
+    [(null? lst) '()]
+    [(char-whitespace? (car lst)) (skip-space (cdr lst))]
+    [else lst]))
+
+
+
+
+; read into a string
+
+(define (read-number lst acc)
+  (cond
+    [(null? lst) (list (list->string (reverse acc)) '())]
+    [(char-numeric? (car lst)) 
+
+     (read-number (cdr lst) (cons (car lst) acc))]
+
+    [else (list (list->string (reverse acc)) lst)]))
+
