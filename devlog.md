0032a8e541c44abac874bbbe4c34f741256d90b9
Himesh A <himesha@mac.local.ftr>
2026-03-06 11:27:35 -0600

    Commit 2: added operations and history

--- devlog.md
+++ devlog.md
@@ -6,3 +6,10 @@ This is my first commit. I made the mistake of starting kind of late because thi
 
 3/05/2026 5:10 pm 
 This is the end of my first work session. I wrote the batch detection and started with the processing of user input. 
+
+3/06/2026 9:53am 
+I am beginning my work session. This session I plan to add the expression functions. I expect this to take a while to complete because I would need to look at documentation online and test out if my code works. 
+
+3/06/2026 11:23am
+This work session took longer than expected. I felt inefficient but I got the job done. I also decided to add the history functionality I think during the next session I will add quit functionality and then I should be done. I should have started working on this earlier because I am feeling burnout right now and need a break. 
+
--- main.rkt
+++ main.rkt
@@ -20,11 +20,7 @@
     [(char-whitespace? (car lst)) (skip-space (cdr lst))]
     [else lst]))
 
-
-
-
-; read into a string
-
+;read string
 (define (read-number lst acc)
   (cond
     [(null? lst) (list (list->string (reverse acc)) '())]
@@ -34,3 +30,87 @@
 
     [else (list (list->string (reverse acc)) lst)]))
 
+
+
+(define (eval-expr chars hist)
+  (let* [(trimmed (skip-space chars))]
+    (cond
+      [(null? trimmed) (list 'error "empty expression")]
+      
+
+
+      ;unary negation
+      [(char=? (car trimmed) #\-)
+       (let* [(arg-res (eval-expr (cdr trimmed) hist))]
+         (if (eq? (car arg-res) 'error)
+             arg-res
+             (list (- (car arg-res)) (cadr arg-res))))]
+             
+
+
+
+      ;bin addition
+      [(char=? (car trimmed) #\+)
+       (let* [(left-res (eval-expr (cdr trimmed) hist))]
+         (if (eq? (car left-res) 'error)
+             left-res
+             (let* [(right-res (eval-expr (cadr left-res) hist))]
+               (if (eq? (car right-res) 'error)
+                   right-res
+                   (list (+ (car left-res) (car right-res)) (cadr right-res))))))]
+                   
+
+
+      ;bin multiplication
+      [(char=? (car trimmed) #\*)
+       (let* [(left-res (eval-expr (cdr trimmed) hist))]
+         (if (eq? (car left-res) 'error)
+             left-res
+             (let* [(right-res (eval-expr (cadr left-res) hist))]
+               (if (eq? (car right-res) 'error)
+                   right-res
+                   (list (* (car left-res) (car right-res)) (cadr right-res))))))]
+                   
+
+      ;bin division
+      [(char=? (car trimmed) #\/)
+       (let* [(left-res (eval-expr (cdr trimmed) hist))]
+         (if (eq? (car left-res) 'error)
+             left-res
+             (let* [(right-res (eval-expr (cadr left-res) hist))]
+               (cond
+                 [(eq? (car right-res) 'error) right-res]
+                 [(= (car right-res) 0) (list 'error "division by zero")] 
+                 [else (list (quotient (car left-res) (car right-res)) (cadr right-res))]))))]
+                 
+
+
+
+
+
+
+      ;history 
+      [(char=? (car trimmed) #\$)
+       (let* [(num-res (read-number (cdr trimmed) '()))
+              (id-str (car num-res))
+              (rest-chars (cadr num-res))]
+         (if (string=? id-str "")
+             (list 'error "missing history id")
+             (let* [(id (string->number id-str))
+                    (rev-hist (reverse hist))] 
+               (if (or (< id 1) (> id (length rev-hist)))
+                   (list 'error "invalid history id")
+                   (list (list-ref rev-hist (- id 1)) rest-chars)))))]
+                   
+      
+      [(char-numeric? (car trimmed))
+       (let* [(num-res (read-number trimmed '()))
+              (val (string->number (car num-res)))
+              (rest-chars (cadr num-res))]
+         (list val rest-chars))]
+         
+      [else (list 'error "invalid character")])))
+
+
+
+    
